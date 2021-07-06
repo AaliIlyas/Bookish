61fdaf583def754922c66188c35159fb6976fa2d
Ilyas <ilyas.ali@softwire.com>
2021-07-06 15:29:27 +0100

    Added books to the books page

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -18,15 +18,65 @@ namespace Bookish.Controllers
             _logger = logger;
         }
 
-        public IActionResult booksdb()
+        public IActionResult Booksdb()
         {
-            var dune = new BooksViewModel();
-            dune.Title = "Dune";
-            dune.Author = "Frank Herbert";
-            dune.NumberOfCopies = 3;
-            dune.Genre = "Sci-Fi";
-            dune.Id = 1;
-            return View(dune);
+            var dune = new BooksViewModel
+            {
+                Title = "Dune",
+                Author = "Frank Herbert",
+                NumberOfCopies = 3,
+                Genre = "Sci-Fi",
+                Id = 1
+            };
+
+            var emma = new BooksViewModel
+            {
+                Title = "Emma",
+                Author = "Jane Austen",
+                NumberOfCopies = 5,
+                Genre = "Classic",
+                Id = 2
+            };
+
+            var oliverTwist = new BooksViewModel
+            {
+                Title = "Oliver Twist",
+                Author = "Charles Dickens",
+                NumberOfCopies = 3,
+                Genre = "Classic",
+                Id = 3
+            };
+
+            var theVeryHungryCaterpillar = new BooksViewModel
+            {
+                Title = "The Very Hungry Caterpillar",
+                Author = "Eric Carle",
+                NumberOfCopies = 4,
+                Genre = "Children",
+                Id = 4
+            };
+
+            var capital = new BooksViewModel
+            {
+                Title = "Capital",
+                Author = "Karl Marx",
+                NumberOfCopies = 1,
+                Genre = "Politics",
+                Id = 5
+            };
+
+            var ofMiceAndMen = new BooksViewModel
+            {
+                Title = "Of Mice and Men",
+                Author = "John Steinbeck",
+                NumberOfCopies = 2,
+                Genre = "Classic",
+                Id = 6
+            };
+
+            var bookList = new List<BooksViewModel>() { dune, emma, oliverTwist, theVeryHungryCaterpillar, capital, ofMiceAndMen };
+
+            return View(bookList);
         }
     }
 }
