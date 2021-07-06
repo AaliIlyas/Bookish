4f7fa38f9dcc42219fe2aa4fc6951875c7d800d8
Cress <cressida.chesworth@softwire.com>
2021-07-06 13:43:06 +0100

    completed part 2

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Bookish.Models;
+
+namespace Bookish.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ILogger<BooksController> _logger;
+
+        public BooksController(ILogger<BooksController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult booksdb()
+        {
+            var books = new BooksViewModel();
+            books.Name = "Dune"; 
+            return View(books);
+        }
+       
+    }
+}
