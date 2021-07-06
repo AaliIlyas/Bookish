bf44b91f49f1f69e72a2449cb8866ef114b6a24e
Ilyas <ilyas.ali@softwire.com>
2021-07-06 16:01:07 +0100

    Created classes for other DB tables

---
+++ Models/CheckedOutBooks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookish.Models
+{
+    public class CheckedOutBooks
+    {
+        public int Name_Id { get; set; }
+        public int Book_Id { get; set; }
+        public int Id { get; set; }
+        public DateTime DateLoaned { get; set; }
+        public DateTime DateDue { get; set; }
+        public DateTime DateReturned { get; set; }
+    }
+}
